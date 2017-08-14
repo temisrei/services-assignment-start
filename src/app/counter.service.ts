f985ebd6c64bd2941bd25cfecce68861ed7cab47
Rei Lin <temisrei@gmail.com>
2017-08-14 23:26:33 +0800

    實作 Counter 功能

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CounterService {
+  activeToInactiveCounter = 0;
+  inactiveToActiveCounter = 0;
 
   constructor() { }
+
+  incrementActiveToInactive() {
+    this.activeToInactiveCounter++;
+    console.log(this.activeToInactiveCounter);
+  }
+
+  incrementInactiveToActive() {
+    this.inactiveToActiveCounter++;
+    console.log(this.inactiveToActiveCounter);
+  }
 
 }
