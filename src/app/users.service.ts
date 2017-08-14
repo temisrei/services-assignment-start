d2b0eaf152acc09a985e4ed2d7b7a8dc27c511a3
Rei Lin <temisrei@gmail.com>
2017-08-14 23:02:44 +0800

    搬移 AppComponent 與 Users 相關屬性與方法到 UsersService

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UsersService {
+  activeUsers = ['Max', 'Anna'];
+  inactiveUsers = ['Chris', 'Manu'];
 
   constructor() { }
+
+  setToActive(id: number) {
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
+  }
+
+  setToInactive(id: number) {
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
+  }
 
 }
