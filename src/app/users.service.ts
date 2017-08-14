3234cf550ceec655a227c31b710b32da8e83f9da
Rei Lin <temisrei@gmail.com>
2017-08-14 23:31:07 +0800

    實作相關 counter 功能。

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -11,11 +11,13 @@ export class UsersService {
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.counterSvc.incrementInactiveToActive();
   }
 
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counterSvc.incrementActiveToInactive();
   }
 
 }
