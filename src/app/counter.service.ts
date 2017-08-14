899ccb8cb7d294ab9567d64ed2b346b4bb0e9366
Rei Lin <temisrei@gmail.com>
2017-08-14 23:33:10 +0800

    稍微修改 CounterService 顯示 Counter 說明。

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -9,12 +9,12 @@ export class CounterService {
 
   incrementActiveToInactive() {
     this.activeToInactiveCounter++;
-    console.log(this.activeToInactiveCounter);
+    console.log(`Active to Inactive: ${this.activeToInactiveCounter}`);
   }
 
   incrementInactiveToActive() {
     this.inactiveToActiveCounter++;
-    console.log(this.inactiveToActiveCounter);
+    console.log(`Inactive to Active: ${this.inactiveToActiveCounter}`);
   }
 
 }
