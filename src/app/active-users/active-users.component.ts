ea93410743c58aaeb9968627b08e40dfda01a22c
Rei Lin <temisrei@gmail.com>
2017-08-14 23:10:25 +0800

    在 ActiveUsersComponent 子元件 注入服務元件

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-active-users',
@@ -8,6 +9,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ActiveUsersComponent {
   @Input() users: string[];
   @Output() userSetToInactive = new EventEmitter<number>();
+
+  constructor(private usersSvc: UsersService) {
+
+  }
 
   onSetToInactive(id: number) {
     this.userSetToInactive.emit(id);
